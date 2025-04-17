57ea5ccb90594d7a1e2c05afada5fb5bef6402ec
Sultanateteam <sultanateteam@gmail.com>
2025-04-17 17:55:23 +0500

    articles card

---
+++ src/components/home/Article.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="py-6 rounded-l shadow border">
+    <div
+      class="mx-auto max-w-7xl px-3 text-center flex flex-col justify-between lg:px-8 h-100"
+    >
+      <div class="mx-auto max-w-2xl lg:mx-0">
+        <div class="border h-40 relative">
+          <small class="absolute top-0 left-2">
+            {{ article.createdAt.slice(0, 10) }}
+          </small>
+          <small class="absolute top-0 right-2">
+            {{ article.createdAt.slice(11, 16) }}
+          </small>
+          <img
+            class="object-fit-fil w-100 h-100"
+            src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=400"
+          />
+        </div>
+        <h3
+          class="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl"
+        >
+          {{ article.title }}
+        </h3>
+        <p class="mt-2 text-lg/8 text-gray-600">
+          {{ article.description }}
+        </p>
+      </div>
+      <div class="flex justify-between">
+        <button type="button" class="btn btn-primary px-3">Read more...</button>
+        <button type="button" class="btn btn-success px-3">Edit</button>
+        <button type="button" class="btn btn-danger px-3">Delete</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Article",
+  props: {
+    article: {
+      type: Object,
+      required: false,
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,27 @@
 <template>
-  <div></div>
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
+      <Article v-for="article in articles" :key="article" :article="article" />
+    </div>
+  </div>
 </template>
 
 <script>
+import Article from "@/components/home/article.vue";
+
 export default {
   name: "home",
+  components: {
+    Article,
+  },
   mounted() {
-    console.log(this.$store)
-    this.$store.dispatch('getArticles')
+    this.$store.dispatch("getArticles");
+  },
+  computed: {
+    articles() {
+      const data = this.$store.state.articles.articles;
+      return Array.isArray(data) ? data : [];
+    },
   },
 };
 </script>
